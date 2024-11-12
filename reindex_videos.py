1a25ad7375b49f19c7e5702c30e745ce99cb509b
ElTinmar <martin.privat@hotmail.fr>
2024-11-12 01:29:41 +0100

    multiprocess

--- reindex_videos.py
+++ reindex_videos.py
@@ -1,18 +1,21 @@
 from video_tools import CPU_VideoProcessor
 from pathlib import Path
-from config import datafolder
+from config import datafolder, n_cores
 import shutil
+from multiprocessing import Pool
 
-for p in datafolder.rglob("*.avi"):
-
+def reindex(p):
     print(p)
-
-    timestamp = p.with_suffix('.txt')
-
+    
+    # reindex videos
     destination = Path(str(p.parent).replace("/data", "/reindexed"))
     destination.mkdir(parents=True, exist_ok=True)
-
     processor = CPU_VideoProcessor(p)
     processor.reindex(suffix=None, dest_folder=destination)
-
+    
+    # copy timestamps as is
+    timestamp = p.with_suffix('.txt')
     shutil.copy(timestamp, destination / timestamp.name)
+
+with Pool(n_cores) as pool:
+    pool.map(reindex, datafolder.rglob("*.avi"))
