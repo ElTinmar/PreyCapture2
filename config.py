3be0c60b815758eb734b9026910153cfba8ad39e
ElTinmar <martin.privat@hotmail.fr>
2024-12-06 00:58:07 +0100

    change path

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import platform
 folder_map = {
     'O1-596': '/media/martin/DATA/Mecp2',
     'TheNewBeast': '/media/martin/MARTIN_8TB_0/Work/Sumbre_New/Mecp2',
-    'O1-619': '/home/martin/Desktop/tracking'
+    'O1-619': '/home/martin/Downloads/Mecp2',
 }
 
 basefolder = Path(folder_map[platform.node()])   
